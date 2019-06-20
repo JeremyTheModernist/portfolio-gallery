3d6287a4f53ca0332faf94a9b7b663b4b2c1f002
JeremyTheModernist <jeremyb1988@gmail.com>
2019-06-20 12:31:34 -0700

    Updating Image Controls to pass image srcs

--- src/components/Image-Medium/Medium-Image.js
+++ src/components/Image-Medium/Medium-Image.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import ItemInformation from '../ItemInformation/ItemInformation.js'
+import ItemInformation from '../ItemInformation/ItemInformation.js';
+import ItemImage from '../ItemImage/ItemImage.js';
 
 function MediumImage(props){
   return(
     <div className="grid__item col-2">
        <ItemInformation id={props.id}/>
+       <ItemImage id={props.id}/>
     </div>
   )
 }
--- src/components/Image-XXL/XXL-Image.js
+++ src/components/Image-XXL/XXL-Image.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ItemInformation from '../ItemInformation/ItemInformation.js'
+import ItemImage from '../ItemImage/ItemImage.js'
 
 function XXLargeImage(props){
   return(
     <div className="grid__item col-6">
-      <ItemInformation id={props.id}/>
+       <ItemInformation id={props.id}/>
+       <ItemImage id={props.id}/>
     </div>
   )
 }
