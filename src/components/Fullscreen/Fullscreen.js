35fee5e7c8316e1a9ad1ad87ae1319c4a92f6e10
JeremyTheModernist <jeremyb1988@gmail.com>
2019-06-21 12:26:38 -0700

    small changes.

--- src/components/Fullscreen/Fullscreen.js
+++ src/components/Fullscreen/Fullscreen.js
@@ -8,7 +8,7 @@ class Fullscreen extends React.Component{
         <div className="fullscreenContent">
           <div className="fullscreenContent__words col-1">
               <h2 className="fullscreenContent__title">{context.selected.title}</h2>
-              <p className="fullscreenContent__description">This image highlight the beautiful details of a flower in bloom. Shot in Hawaii, painted in Alabama.</p>
+              <p className="fullscreenContent__description">This image highlights the beautiful details of a flower in bloom. Shot in Hawaii, painted in Alabama.</p>
               <a className="fullscreenContent__link">Contact for More Info</a>
           </div>
           <div className="fullscreenContent__image col-4">
