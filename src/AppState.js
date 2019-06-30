b66e8605453994d30b973110f3a4b2d4d16808ae
JeremyTheModernist <jeremyb1988@gmail.com>
2019-06-30 12:49:04 -0700

    More adjustments to image sizes.

--- src/AppState.js
+++ src/AppState.js
@@ -18,11 +18,11 @@ class AppState extends React.Component{
           title:'Blue and Gold Macaw',
           subtitle: 'Watercolor Painting Original 16 x 20 Giclees available',
           description: 'Watercolor Painting Original 16 x 20 Giclees available',
-          src: 'https://res.cloudinary.com/gestalt/image/upload/v1561668776/painters-folio/Vertical/Colonel_-_Blue_and_Gold_Macaw_Brevard_Zoo_tptwuv.jpg',
+          src: 'https://res.cloudinary.com/gestalt/image/upload/w_850,q_60/v1561668776/painters-folio/Vertical/Colonel_-_Blue_and_Gold_Macaw_Brevard_Zoo_tptwuv.jpg',
           breakpointImgs: {
             large: 'https://res.cloudinary.com/gestalt/image/upload/w_600,q_60/v1561668776/painters-folio/Vertical/Colonel_-_Blue_and_Gold_Macaw_Brevard_Zoo_tptwuv.jpg',
             medium: 'https://res.cloudinary.com/gestalt/image/upload/w_600,q_60/v1561668776/painters-folio/Vertical/Colonel_-_Blue_and_Gold_Macaw_Brevard_Zoo_tptwuv.jpg',
-            small: 'https://res.cloudinary.com/gestalt/image/upload/w_600,q_60/v1561668776/painters-folio/Vertical/Colonel_-_Blue_and_Gold_Macaw_Brevard_Zoo_tptwuv.jpg',
+            small: 'https://res.cloudinary.com/gestalt/image/upload/w_500,q_60/v1561668776/painters-folio/Vertical/Colonel_-_Blue_and_Gold_Macaw_Brevard_Zoo_tptwuv.jpg',
           }
         }
       },
@@ -48,7 +48,7 @@ class AppState extends React.Component{
           description: 'Watercolor Painting Original 16 x 20 Giclees available',
           src: 'https://res.cloudinary.com/gestalt/image/upload/w_1000,q_60/v1561668763/painters-folio/Horizontal/Church_on_the_North_Shore_II_dislat.jpg',
           breakpointImgs: {
-            large: 'https://res.cloudinary.com/gestalt/image/upload/w_1100,q_60//v1561668763/painters-folio/Horizontal/Church_on_the_North_Shore_II_dislat.jpg',
+            large: 'https://res.cloudinary.com/gestalt/image/upload/w_1100,q_60/v1561668763/painters-folio/Horizontal/Church_on_the_North_Shore_II_dislat.jpg',
             medium: 'https://res.cloudinary.com/gestalt/image/upload/w_800,q_60/v1561668763/painters-folio/Horizontal/Church_on_the_North_Shore_II_dislat.jpg',
             small: 'https://res.cloudinary.com/gestalt/image/upload/w_600,q_60//v1561668763/painters-folio/Horizontal/Church_on_the_North_Shore_II_dislat.jpg'
           }
@@ -104,9 +104,9 @@ class AppState extends React.Component{
           description: 'Watercolor Painting Original 28 x 22 Giclees available',
           src: 'https://res.cloudinary.com/gestalt/image/upload/v1561668765/painters-folio/Horizontal/Key_West_Christmas_Palm_gwvddk.jpg',
           breakpointImgs: {
-            large: 'https://res.cloudinary.com/gestalt/image/upload/v1561668765/painters-folio/Horizontal/Key_West_Christmas_Palm_gwvddk.jpg',
-            medium: 'https://res.cloudinary.com/gestalt/image/upload/v1561668765/painters-folio/Horizontal/Key_West_Christmas_Palm_gwvddk.jpg',
-            small: 'https://res.cloudinary.com/gestalt/image/upload/v1561668765/painters-folio/Horizontal/Key_West_Christmas_Palm_gwvddk.jpg',
+            large: 'https://res.cloudinary.com/gestalt/image/upload/w_1100,q_40/v1561668765/painters-folio/Horizontal/Key_West_Christmas_Palm_gwvddk.jpg',
+            medium: 'https://res.cloudinary.com/gestalt/image/upload/w_800,q_60/v1561668765/painters-folio/Horizontal/Key_West_Christmas_Palm_gwvddk.jpg',
+            small: 'https://res.cloudinary.com/gestalt/image/upload/w_600,q_60/v1561668765/painters-folio/Horizontal/Key_West_Christmas_Palm_gwvddk.jpg',
           }
         }
       },
@@ -174,9 +174,9 @@ class AppState extends React.Component{
           description: 'Watercolor Painting Original 16 X 20 Giclees available',
           src: 'https://res.cloudinary.com/gestalt/image/upload/v1561668783/painters-folio/Vertical/Courtyard_in_Poipu_h0xxzo.jpg',
           breakpointImgs: {
-            large: 'https://res.cloudinary.com/gestalt/image/upload/v1561668783/painters-folio/Vertical/Courtyard_in_Poipu_h0xxzo.jpg',
-            medium: 'https://res.cloudinary.com/gestalt/image/upload/v1561668783/painters-folio/Vertical/Courtyard_in_Poipu_h0xxzo.jpg',
-            small: 'https://res.cloudinary.com/gestalt/image/upload/v1561668783/painters-folio/Vertical/Courtyard_in_Poipu_h0xxzo.jpg',
+            large: 'https://res.cloudinary.com/gestalt/image/upload/w_900,q_40/v1561668783/painters-folio/Vertical/Courtyard_in_Poipu_h0xxzo.jpg',
+            medium: 'https://res.cloudinary.com/gestalt/image/upload/w_700,q_60/v1561668783/painters-folio/Vertical/Courtyard_in_Poipu_h0xxzo.jpg',
+            small: 'https://res.cloudinary.com/gestalt/image/upload/w_600,q_50/v1561668783/painters-folio/Vertical/Courtyard_in_Poipu_h0xxzo.jpg',
           }
         }
       },
@@ -186,11 +186,11 @@ class AppState extends React.Component{
           title:'Cabana Canopy',
           subtitle: 'Watercolor Painting Original 16 x 20 Giclees available',
           description: 'Watercolor Painting Original 16 x 20 Giclees available',
-          src: 'https://res.cloudinary.com/gestalt/image/upload/v1561668785/painters-folio/Vertical/Cabana_Canopy_ezfyuh.jpg',
+          src: 'https://res.cloudinary.com/gestalt/image/upload/w_850,q_60/v1561668785/painters-folio/Vertical/Cabana_Canopy_ezfyuh.jpg',
           breakpointImgs: {
-            large: 'https://res.cloudinary.com/gestalt/image/upload/v1561668785/painters-folio/Vertical/Cabana_Canopy_ezfyuh.jpg',
-            medium: 'https://res.cloudinary.com/gestalt/image/upload/v1561668785/painters-folio/Vertical/Cabana_Canopy_ezfyuh.jpg',
-            small: 'https://res.cloudinary.com/gestalt/image/upload/v1561668785/painters-folio/Vertical/Cabana_Canopy_ezfyuh.jpg',
+            large: 'https://res.cloudinary.com/gestalt/image/upload/w_600,q_60/v1561668785/painters-folio/Vertical/Cabana_Canopy_ezfyuh.jpg',
+            medium: 'https://res.cloudinary.com/gestalt/image/upload/w_600,q_60/v1561668785/painters-folio/Vertical/Cabana_Canopy_ezfyuh.jpg',
+            small: 'https://res.cloudinary.com/gestalt/image/upload/w_600,q_60/v1561668785/painters-folio/Vertical/Cabana_Canopy_ezfyuh.jpg',
           }
         }
       },
@@ -200,11 +200,11 @@ class AppState extends React.Component{
           title:'Pineapple on the Banana River',
           subtitle: 'Watercolor Painting Original 16 x 20 Giclees available',
           description: 'Watercolor Painting Original 16 x 20 Giclees available',
-          src: 'https://res.cloudinary.com/gestalt/image/upload/v1561668781/painters-folio/Vertical/Pineapple_on_the_Banana_River_g8tglx.jpg',
+          src: 'https://res.cloudinary.com/gestalt/image/upload/w_850,q_60/v1561668781/painters-folio/Vertical/Pineapple_on_the_Banana_River_g8tglx.jpg',
           breakpointImgs: {
-            large: 'https://res.cloudinary.com/gestalt/image/upload/v1561668781/painters-folio/Vertical/Pineapple_on_the_Banana_River_g8tglx.jpg',
-            medium: 'https://res.cloudinary.com/gestalt/image/upload/v1561668781/painters-folio/Vertical/Pineapple_on_the_Banana_River_g8tglx.jpg',
-            small: 'https://res.cloudinary.com/gestalt/image/upload/v1561668781/painters-folio/Vertical/Pineapple_on_the_Banana_River_g8tglx.jpg',
+            large: 'https://res.cloudinary.com/gestalt/image/upload/w_600,q_60/v1561668781/painters-folio/Vertical/Pineapple_on_the_Banana_River_g8tglx.jpg',
+            medium: 'https://res.cloudinary.com/gestalt/image/upload/w_600,q_60/v1561668781/painters-folio/Vertical/Pineapple_on_the_Banana_River_g8tglx.jpg',
+            small: 'https://res.cloudinary.com/gestalt/image/upload/w_500,q_60/v1561668781/painters-folio/Vertical/Pineapple_on_the_Banana_River_g8tglx.jpg',
           }
         }
       },
@@ -230,9 +230,9 @@ class AppState extends React.Component{
           description: 'Acrylic Painting Original 20 x 16 Giclees available',
           src: 'https://res.cloudinary.com/gestalt/image/upload/v1561668769/painters-folio/Horizontal/Hoos_Hideout_ztwc19.jpg',
           breakpointImgs: {
-            large: 'https://res.cloudinary.com/gestalt/image/upload/v1561668769/painters-folio/Horizontal/Hoos_Hideout_ztwc19.jpg',
-            medium: 'https://res.cloudinary.com/gestalt/image/upload/v1561668769/painters-folio/Horizontal/Hoos_Hideout_ztwc19.jpg',
-            small: 'https://res.cloudinary.com/gestalt/image/upload/v1561668769/painters-folio/Horizontal/Hoos_Hideout_ztwc19.jpg',
+            large: 'https://res.cloudinary.com/gestalt/image/upload/w_1100,q_40/v1561668769/painters-folio/Horizontal/Hoos_Hideout_ztwc19.jpg',
+            medium: 'https://res.cloudinary.com/gestalt/image/upload/w_800,q_60/v1561668769/painters-folio/Horizontal/Hoos_Hideout_ztwc19.jpg',
+            small: 'https://res.cloudinary.com/gestalt/image/upload/w_600,q_60/v1561668769/painters-folio/Horizontal/Hoos_Hideout_ztwc19.jpg',
           }
         }
       },
@@ -244,9 +244,9 @@ class AppState extends React.Component{
           description: 'Watercolor Painting Original 20 x 16 Giclees available',
           src: 'https://res.cloudinary.com/gestalt/image/upload/v1561668771/painters-folio/Horizontal/The_Lure_of_Leaves_-_ZAPP_huw1j4.jpg',
           breakpointImgs: {
-            large: 'https://res.cloudinary.com/gestalt/image/upload/v1561668771/painters-folio/Horizontal/The_Lure_of_Leaves_-_ZAPP_huw1j4.jpg',
-            medium: 'https://res.cloudinary.com/gestalt/image/upload/v1561668771/painters-folio/Horizontal/The_Lure_of_Leaves_-_ZAPP_huw1j4.jpg',
-            small: 'https://res.cloudinary.com/gestalt/image/upload/v1561668771/painters-folio/Horizontal/The_Lure_of_Leaves_-_ZAPP_huw1j4.jpg',
+            large: 'https://res.cloudinary.com/gestalt/image/upload/w_1100,q_40/v1561668771/painters-folio/Horizontal/The_Lure_of_Leaves_-_ZAPP_huw1j4.jpg',
+            medium: 'https://res.cloudinary.com/gestalt/image/upload/w_800,q_60/v1561668771/painters-folio/Horizontal/The_Lure_of_Leaves_-_ZAPP_huw1j4.jpg',
+            small: 'https://res.cloudinary.com/gestalt/image/upload/w_600,q_60/v1561668771/painters-folio/Horizontal/The_Lure_of_Leaves_-_ZAPP_huw1j4.jpg',
           }
         }
       },
@@ -284,11 +284,11 @@ class AppState extends React.Component{
           title:'Naturometry',
           subtitle: '2019 43rd Annual Montgomery Art Guild Museum Exhibition Montgomery, Alabama',
           description: 'Watercolor Painting Original 16 x 20 Giclees available',
-          src: 'https://res.cloudinary.com/gestalt/image/upload/v1561668782/painters-folio/Vertical/Naturometry_hm7k1i.jpg',
+          src: 'https://res.cloudinary.com/gestalt/image/upload/w_850,q_60/v1561668782/painters-folio/Vertical/Naturometry_hm7k1i.jpg',
           breakpointImgs: {
-            large: 'https://res.cloudinary.com/gestalt/image/upload/v1561668782/painters-folio/Vertical/Naturometry_hm7k1i.jpg',
-            medium: 'https://res.cloudinary.com/gestalt/image/upload/v1561668782/painters-folio/Vertical/Naturometry_hm7k1i.jpg',
-            small: 'https://res.cloudinary.com/gestalt/image/upload/v1561668782/painters-folio/Vertical/Naturometry_hm7k1i.jpg',
+            large: 'https://res.cloudinary.com/gestalt/image/upload/w_600,q_60/v1561668782/painters-folio/Vertical/Naturometry_hm7k1i.jpg',
+            medium: 'https://res.cloudinary.com/gestalt/image/upload/w_600,q_60/v1561668782/painters-folio/Vertical/Naturometry_hm7k1i.jpg',
+            small: 'https://res.cloudinary.com/gestalt/image/upload/w_500,q_60/v1561668782/painters-folio/Vertical/Naturometry_hm7k1i.jpg',
           }
         }
       }
--- src/components/ItemImage/ItemImage.js
+++ src/components/ItemImage/ItemImage.js
@@ -9,7 +9,7 @@ function ItemImage(props){
           return context.information.map((item) => {
               return props.id == item.id ?
                 <div className="item__image" key={item.id}
-                  onClick={() => context.toggleFullscreen({src:item.content.breakpointImgs.large,title:item.content.title,description:item.content.description})}>
+                  onClick={() => context.toggleFullscreen({src:item.content.src,title:item.content.title,description:item.content.description})}>
                   <picture>
                     <source media="(min-width:1024px)" srcset={`${item.content.breakpointImgs.large}`}/>
                     <source media="(min-width:500px)" srcset={`${item.content.breakpointImgs.medium}`}/>
